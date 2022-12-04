2aa160942a561ab85ae2572a40a5349c40e1a88a
Ion Catana <90708252+IonCatana@users.noreply.github.com>
2022-12-04 18:19:49 +0100

    some fix

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// EventListener la svg gira quando si scolla l'intera lunghezza della pagina
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
